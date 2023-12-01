0cbd871d3bfd24ee78d698c8fc494e528fb1c189
arefehamidi.guilan <arefehamidi.guilan@gmail.com>
2023-12-01 11:26:01 +0330

    create project

--- Code/02 Essentials/14 Finished/app/routes/index.jsx
+++ Code/02 Essentials/14 Finished/app/routes/index.jsx
@@ -3,13 +3,18 @@ import { Link } from '@remix-run/react';
 import homeStyles from '~/styles/home.css';
 
 export default function Index() {
+  const tryButton= styled.p`
+  a:hover {
+    background-color: var(--color-secondary-200);
+  }
+  `
   return (
     <main id="content">
       <h1>A better way of keeping track of your notes</h1>
       <p>Try our early beta and never loose track of your notes again!</p>
-      <p id="cta">
+      <tryButton>
         <Link to="/notes">Try Now!</Link>
-      </p>
+      </tryButton>
     </main>
   );
 }
